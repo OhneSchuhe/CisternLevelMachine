54e92ca8388afc9978bd62e793c4379de9426439
OhneSchuhe <thycore+githubaccount@gmail.com>
2022-07-08 21:51:00 +0200

    Added pw.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFiManager.h>   
 #include <MQTTClient.h>
+#include <pw.h>
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,7 +15,7 @@ void setup() {
   // init mqtt client
   MQTTClient client;
   Serial.print("\nconnecting to MQTT broker...");
-  while (!client.connect("arduino", "public", "public")) {
+  while (!client.connect("D1 Mini Cistern", mqttusr, mqttpass)) {
     Serial.print(".");
     delay(1000);
   }
